0475009b228f0bb4aa2194fc24f0133609f25ef6
Adam Simpson <adam.simpson@gm.com>
2019-01-23 14:24:04 -0600

    finished natural features

--- js/wil-hunting.js
+++ js/wil-hunting.js
@@ -1,27 +1,67 @@
 //Foraging & Hunting page 15
 let forageData = [[[[30,18],[30,18],[29,20],[27,21]],[[27,21],[27,18],[27,20],[25,21]],[[30,21],[30,18],[30,21],[30,21]],[[30,17],[30,15],[30,18],[30,21]],[[18,12],[21,15],[18,12],[15,12]],[[1,6],[0,0],[1,8],[1,8]],[[29,15],[30,15],[29,15],[27,18]]],[[[27,21],[30,22],[29,22],[24,23]],[[16,21],[24,21],[18,18],[12,18]],[[30,21],[30,21],[30,21],[27,21]],[[27,21],[30,21],[30,20],[24,21]],[[15,15],[15,15],[15,12],[12,12]],[[1,5],[0,0],[1,9],[1,9]],[[27,18],[30,18],[29,18],[24,18]]],[[[22,18],[27,20],[24,15],[20,15]],[[9,18],[12,18],[9,15],[6,18]],[[27,18],[30,18],[27,18],[24,18]],[[18,18],[22,21],[18,15],[15,18]],[[10,18],[12,15],[10,12],[9,12]],[[2,15],[1,9],[2,15],[3,7]],[[20,18],[24,18],[20,15],[15,15]]],[[[9,15],[12,18],[8,15],[6,15]],[[5,15],[6,18],[5,15],[3,15]],[[27,18],[30,15],[24,18],[21,18]],[[9,18],[15,18],[9,15],[6,18]],[[6,12],[9,12],[6,15],[5,12]],[[2,15],[5,15],[2,5],[0,0]],[[10,15],[15,18],[10,15],[7,12]]],[[[3,12],[3,12],[2,15],[2,6]],[[0,0],[2,15],[0,0],[0,0]],null,[[2,15],[3,15],[2,15],[1,15]],null,[[2,5],[3,5],[0,0],[0,0]],[[3,15],[5,15],[3,6],[3,6]]]];
 let huntData = [[[15,15,15,15],[12,12,12,12],[15,15,15,15],[15,15,15,15],[15,15,15,15],[2,2,2,2],[15,15,15,15]],[[15,15,15,15],[10,10,10,10],[15,15,15,15],[15,15,15,15],[15,15,15,12],[2,1,2,3],[15,15,15,18]],[[12,12,12,8],[9,9,9,5],[15,15,15,12],[12,15,15,8],[10,15,15,5],[2,1,2,3],[12,15,15,6]],[[3,5,5,1],[2,3,2,1],[3,3,1,1],[3,5,5,1],[3,5,3,1],[3,3,2,1],[4,6,6,2]],[[3,3,2,1],[1,2,1,1],null,[2,3,2,1],null,[1,3,1,1],[1,3,1,1]]];
-function forage(climate,season,terrain) {
-    let data = forageData[climate][terrain][season];
-    let result = "found ";
-    if (data[0] < rollDie(1,30)) {
-        result += "nothing";
+let medCnt = [2,3,6,8];
+let smallCnt = [3,6,8,12];
+function hntr2(num) {
+    if (num < 4) {
+        num = 1;
+    } else if (num < 9) {
+        num = 2;
+    } else if (num < 16) {
+        num = 3;
+    } else if (num < 23) {
+        num = 4;
+    } else if (num < 28) {
+        num = 5;
     } else {
-        if (data[1] < rollDie(1,30)) {
-            result += "nothing edible";
-        } else {
-            result += "food";
-        }
+        num = 6;
     }
-    document.getElementById("forageRslt").innerHTML = result;
+    return num;
 }
-function hunt(climate,season,terrain) {
-    let data = huntData[climate][terrain][season];
-    let result = "";
-    if (data < rollDie(1,30)) {
-        result += "no game found";
+function hntr3(num) {
+    if (num < 3) {
+        num = 2;
+    } else if (num < 6) {
+        num = 3;
+    } else if (num < 11) {
+        num = 4;
+    } else if (num < 19) {
+        num = 5;
+    } else if (num < 24) {
+        num = 6;
+    } else if (num < 29) {
+        num = 7;
+    } else {
+        num = 8;
     }
-    document.getElementById("huntRslt").innerHTML = result;
+    return num;
+}
+function hntr4(num) {
+    if (num < 2) {
+        num = 2;
+    } else if (num < 4) {
+        num = 3;
+    } else if (num < 6) {
+        num = 4;
+    } else if (num < 9) {
+        num = 5;
+    } else if (num < 13) {
+        num = 6;
+    } else if (num < 19) {
+        num = 7;
+    } else if (num < 23) {
+        num = 8;
+    } else if (num < 26) {
+        num = 9;
+    } else if (num < 28) {
+        num = 10;
+    } else if (num < 30) {
+        num = 11;
+    } else {
+        num = 12;
+    }
+    return num;
 }
 $("input[name=climatePck]").change(function () {
     let cond1 = $("#cond1");
@@ -41,11 +81,82 @@ $("#forageRoll").click(function () {
     let climate = parseInt($("input[name=climatePck]:checked").val());
     let season = parseInt($("input[name=seasonPck]:checked").val());
     let terrain = parseInt($("input[name=terrainPck]:checked").val());
-    forage(climate,season,terrain);
+    let data = forageData[climate][terrain][season];
+    let result = "found ";
+    if (data[0] < rollDie(1,30)) {
+        result += "nothing";
+    } else {
+        if (data[1] < rollDie(1,30)) {
+            result += "nothing edible";
+        } else {
+            result += "food";
+        }
+    }
+    document.getElementById("forageRslt").innerHTML = result;
 });
 $("#huntRoll").click(function () {
     let climate = parseInt($("input[name=climatePck]:checked").val());
     let season = parseInt($("input[name=seasonPck]:checked").val());
     let terrain = parseInt($("input[name=terrainPck]:checked").val());
-    hunt(climate,season,terrain);
+    let data = huntData[climate][terrain][season];
+    let hntSuc = [];
+    let i,success,gameType,gameSize,maxCnt,result;
+    let distance = rollDie(1,3) * 10;
+    let huntrCnt = parseInt($("#huntOp").val());
+    if (huntrCnt > 1) {
+        data = data - ((huntrCnt - 1) * 3);
+    }
+    if ($("#offHours").prop('checked') === true) {
+        data -= 3;
+    }
+    if ($("#isProficient").prop('checked') === true) {
+        data += 3;
+    }
+    for (i=0;i<huntrCnt;i++) {
+        if (data >= rollDie(1,30)) {
+            success = true;
+        }
+    }
+    if (!success) {
+        result = "no game found";
+    } else {
+        gameType = rollDie(1,10);
+        if (gameType > 8) {
+            gameSize = "large";
+            distance += 50;
+            maxCnt = rollDie(2,3);
+        } else if (gameType < 5) {
+            gameSize = "small";
+            distance += 10;
+            maxCnt = smallCnt[rollDie(0,3)];
+        } else {
+            gameSize = "medium";
+            distance += 30;
+            maxCnt = medCnt[rollDie(0,3)];
+        }
+        switch (huntrCnt) {
+            case 1:
+                hntSuc[0] = rollDie(1,3);
+                hntSuc[1] = rollDie(1,3);
+                break;
+            case 2:
+                hntSuc[0] = hntr2(rollDie(1,30));
+                hntSuc[1] = hntr2(rollDie(1,30));
+                break;
+            case 3:
+                hntSuc[0] = hntr3(rollDie(1,30));
+                hntSuc[1] = hntr3(rollDie(1,30));
+                break;
+            default:
+                hntSuc[0] = hntr4(rollDie(1,30));
+                hntSuc[1] = hntr4(rollDie(1,30));
+        }
+        hntSuc.sort(function(a, b){return a-b});
+        //compute number of game caught
+        if (maxCnt < hntSuc[0]) {
+            hntSuc[0] = maxCnt;
+        }
+        result = hntSuc[0] + " " + gameSize + " game caught (if your missiles can reach " + distance + " yards). " + hntSuc[1] + " missiles fired.";
+    }
+    document.getElementById("huntRslt").innerHTML = result;
 });
---
+++ js/wil-natFeature.js
@@ -0,0 +1,80 @@
+//Natural Features pages 16 & 17
+hillFeat = ["cave, fracture &mdash; formed when layers of more soluble minerals, such as gypsum, dissolve out from between layers of less soluble rock; rocks fracture and collapse in blocks of stone","cave, lava tube &mdash; formed through volcanic activity","cave, limestone &mdash; solutional cave where rock has been dissolved by natural acid in groundwater seeping through bedding-planes, faults, joints, etc.","cave, talus &mdash; opening between rocks that have fallen down into a pile, often at the base of cliffs","rock shelter","bluff","butte","crag","cuesta (w/ cliff) &mdash; ridge with a gentle slope on one side and a cliff on the other","dell &mdash; small retired valley; dale","draw &mdash; formed by two parallel ridges or spurs with low ground in between them","escarpment &mdash; wide steep slope","esker &mdash; narrow ridge of gravelly and sandy drift deposited by a stream in association with glacier ice","glen","gulch","hillock/knoll &mdash; small hill","lake","lava dome &mdash; roughly circular mound-shaped protrusion resulting from the slow extrusion of viscous lava from a volcano","lava lake &mdash; large volume of molten lava contained in a volcanic vent, crater, or depression","limestone pavement &mdash; flat, incised surface of exposed limestone resembling artificial pavement","mesa","plateau","pond","rock outcropping","spring, artesian &mdash; confined aquifer wish flows upward naturally without the need for pumping","spring, hot","strath &mdash; river valley of considerable size","terrace","tor &mdash; rocky peak","well"];
+mountFeat = ["cave, fracture &mdash; formed when layers of more soluble minerals, such as gypsum, dissolve out from between layers of less soluble rock; rocks fracture and collapse in blocks of stone","cave, lava tube &mdash; formed through volcanic activity","cave, limestone &mdash; solutional cave where rock has been dissolved by natural acid in groundwater seeping through bedding-planes, faults, joints, etc.","cave, talus &mdash; opening between rocks that have fallen down into a pile, often at the base of cliffs","rock shelter","bluff","cliff","crag","crater lake &mdash; lake formed in collapsed volcanic mountain","dry lake","fluvial terrace &mdash; elongated terrace that flanks the side of floodplain or fluvial valley","geyser","gorge","gulch","hoodoo (tent rock) &mdash; tall, thin spire of rock that protrudes from the bottom of an arid drainage basin or badland","kettle (lake) &mdash; shallow, sediment-filled body of water formed by retreating glaciers or draining floodwaters","lava dome &mdash; roughly circular mound-shaped protrusion resulting from the slow extrusion of viscous lava from a volcano","lava lake &mdash; large volume of molten lava contained in a volcanic vent, crater, or depression","ledge","mountain pass","natural arch","pseudocrater &mdash; volcanic landform resembling true volcanic crater, but is not an actual vent from which lava has erupted","ravine","ridge","spring","summit","valley","volcanic cone &mdash; formation in the shape of a cone with a central cater","volcanic crater &mdash; circular depression in the ground caused by volcanic activity","volcanic crater &mdash; opening in ground through which molten lava, ash, and/or gases are ejected"];
+forestFeat = ["cave, fracture &mdash; formed when layers of more soluble minerals, such as gypsum, dissolve out from between layers of less soluble rock; rocks fracture and collapse in blocks of stone","cave, limestone &mdash; solutional cave where rock has been dissolved by natural acid in groundwater seeping through bedding-planes, faults, joints, etc.","cave, talus &mdash; opening between rocks that have fallen down into a pile, often at the base of cliffs","den tree &mdash; tree with cavities suitable for habitation, usually by birds or mammals","rock shelter","wolf tree &mdash; very large tree with wide-spreading crown that has inhibited/prevented the growth of smaller trees around it","brook","clearing (natural)","cloud forest (fog) &mdash; moist forest characterized by a persistent, frequent or seasonal low-level cloud cover, usually at the canopy level","copse &mdash; thicket or grove of small trees","crooked trees &mdash; unexplained grove of twisted and/or deformed trees","dead forest","forked trees","gully","grotto","heavy canopy","heavy underbrush","lake","primeval forest &mdash; forest that has attained great age without significant disturbance","pond","rock outcropping","rot, widespread","sinkhole","spring, artesian &mdash; confined aquifer wish flows upward naturally without the need for pumping","spring, hot","stream","sylvan grove","sylvan forest","thicket","well"];
+plainFeat = ["cave, fracture &mdash; formed when layers of more soluble minerals, such as gypsum, dissolve out from between layers of less soluble rock; rocks fracture and collapse in blocks of stone","cave, limestone &mdash; solutional cave where rock has been dissolved by natural acid in groundwater seeping through bedding-planes, faults, joints, etc.","aquifer &mdash; underground layer of water-bearing permeable rock or unconsolidated materials (gravel, sand, or silt) from which groundwater can be extracted using a water well; 1-in-3 chance naturally tapped","butte","crater lake &mdash; lake formed in collapsed volcanic mountain","dry lake","escarpment &mdash; wide steep slope","esker &mdash; narrow ridge of gravelly and sandy drift deposited by a stream in association with glacier ice","gulch","heavy shrubs","heavy underbrush","hillock/knoll &mdash; small hill","lake","lava dome &mdash; roughly circular mound-shaped protrusion resulting from the slow extrusion of viscous lava from a volcano","limestone pavement &mdash; flat, incised surface of exposed limestone resembling artificial pavement","mesa","mud pits","plateau","pond","ridge","riparian zone &mdash; interface between land and a river or stream","rock outcropping","rock shelter","sinkhole","strath &mdash; river valley of considerable size","tar pit","thicket","tor &mdash; rocky peak","vernal pool &mdash; temporary pools of water providing habitat for distinctive plants or animals","well"];
+swampFeat = ["crooked trees &mdash; unexplained grove of twisted and/or deformed trees","cypress dome &mdash; type of swamp, typically dominated by pond cypress","dam, natural","heavy algae","heavy canopy","heavy mossing","heavy muck","heavy pathogens, air","heavy pathogens, surface","heavy pathogens, water","heavy plants, ermergent","heavy plants, floating","heavy plants, submerged","island, small","island, medium","island, large","islands, small (scatters)","natural gas vent &mdash; highly flammable; susceptible to combustion in presence of open flame","marsh gas &mdash; highly flammable; susceptible to combustion in presence of open flame","mud flat","quicksand","rocky area","rot, widespread","shallow water","slough/channel","spring, acidic hot","tar pit","turbid water &mdash; water in which particulate matter (both inorganic and organic waste) is suspended","volcanic crater &mdash; opening in ground through which molten lava, ash, and/or gases are ejected","volcanic vent w/ tuff ring &mdash; as volcanic vent, with additional outer ring of fragmented layers of volcanic material"];
+desertFeat = ["cave, limestone &mdash; solutional cave where rock has been dissolved by natural acid in groundwater seeping through bedding-planes, faults, joints, etc.","cave, talus &mdash; opening between rocks that have fallen down into a pile, often at the base of cliffs","aquifer &mdash; underground layer of water-bearing permeable rock or unconsolidated materials (gravel, sand, or silt) from which groundwater can be extracted using a water well; 1-in-3 chance naturally tapped","bedrock outcrop","bluff","butte","cacti","chasm","crag","crater lake, dry &mdash; lake formed in collapsed volcanic mountain","cuesta (w/cliff) &mdash; ridge with a gentle slope on one side and a cliff on the other","dunes","escarpment &mdash; wide steep slope","esker &mdash; narrow ridge of gravelly and sandy drift deposited by a stream in association with glacier ice","gulch","hoodoo (tent rock) &mdash; tall, thin spire of rock that protrudes from the bottom of an arid drainage basin or badland","mesa","limestone pavement &mdash; flat, incised surface of exposed limestone resembling artificial pavement","quicksand","oasis","pediment","playa &mdash; broad, level spot where water temporarily accumulates after rains","rock shelter","sailing stones","sinkhole","shrubs","terrace","trees","valley","well, dry"];
+coastFeat = ["cave, sea","barachois &mdash; a coastal lagoon separated from the ocean by a sand or shingle bar","bay","bight &mdash; sharp bend in the coast","blowout &mdash; a sandy depression formed by winds and lack of vegetation","bog","coastal dunes","cove","estuary","firth","fjord","kelp bed (subtidal)","lagoon","lake/loch","marine meadow","mangrove swamp &mdash; area where evergreen trees and shrubs thrive in tideland mud or sand flats inundated daily with sea water","mud flat","quicksand","rocky shore","sand beach","sand flat","salt flat","salt marsh","salt meadow","salting","sea loch (inlet)","seabed (subtidal)","shingle beach &mdash; beach formed by small- to medium-sized cobbles (as opposed to fine sand)","pebble beach &mdash; beach formed by pebbles (as opposed to fine sand)","tide pool"];
+$("#featRoll").click(function () {
+    let terrain = parseInt($("input[name=terrainPck]:checked").val());
+    let roll = rollDie(0,29);
+    let result = "no variation";
+    switch (terrain) {
+        case 0: //hills
+            if (rollDie(1,30) < 7) {
+                result = hillFeat[roll];
+                if (roll < 5) {
+                    if (rollDie(1,2) < 2) {
+                        result += " &mdash; houses a lair";
+                    }
+                }
+            }
+            break;
+        case 1: //mountains
+            if (rollDie(1,30) < 11) {
+                result = mountFeat[roll];
+                if (roll < 5) {
+                    if (rollDie(1,2) < 2) {
+                        result += " &mdash; houses a lair";
+                    }
+                }
+            }
+            break;
+        case 2: //forest
+            if (rollDie(1,30) < 9) {
+                result = forestFeat[roll];
+                if (roll < 6) {
+                    if (rollDie(1,2) < 2) {
+                        result += " &mdash; houses a lair";
+                    }
+                }
+            }
+            break;
+        case 3: //plains
+            if (rollDie(1,30) < 5) {
+                result = plainFeat[roll];
+                if (roll < 2) {
+                    if (rollDie(1,2) < 2) {
+                        result += " &mdash; houses a lair";
+                    }
+                }
+            }
+            break;
+        case 4: //swamp
+            if (rollDie(1,30) < 18) {
+                result = swampFeat[roll];
+            }
+            break;
+        case 5: //desert
+            if (rollDie(1,30) < 5) {
+                result = desertFeat[roll];
+                if (roll < 2) {
+                    if (rollDie(1,2) < 2) {
+                        result += " &mdash; houses a lair";
+                    }
+                }
+            }
+            break;
+        default: //coast
+            if (rollDie(1,30) < 10) {
+                result = coastFeat[roll];
+                if (roll < 1) {
+                    if (rollDie(1,2) < 2) {
+                        result += " &mdash; houses a lair";
+                    }
+                }
+            }
+    }
+    document.getElementById("featRslt").innerHTML = result;
+});
